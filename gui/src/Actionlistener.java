578402b1c853d8b635790c48b37b1d6780f629bd
jatin-js <jatinhmu@gmail.com>
2020-03-20 09:21:42 +0530

    GUI using java

--- gui/src/Actionlistener.java
+++ gui/src/Actionlistener.java
@@ -1,16 +1,37 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+class Demo extends JFrame implements ActionListener {
+    JButton b1;
 
-public class Actionlistener extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
+    Demo(){
+        this.setLayout(null);
+        b1 = new JButton("Button 1");
+        b1.setBounds(130, 5, 100, 50);
+        this.add(b1);
+        b1.addActionListener(this);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    // Override Method
+    public void actionPerformed(ActionEvent evt){
+        if(evt.getSource() == b1){
+            JOptionPane.showMessageDialog(this, "Enter a valid Number", "WARNING", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(this, "Enter a valid String", "ERROR", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(this,"Do you want to quit", "Question", JOptionPane.QUESTION_MESSAGE);
+            JOptionPane.showMessageDialog(this,"You pressed Button FOUR", "INFORMATION", JOptionPane.INFORMATION_MESSAGE);
+
+        }
+    }
+}
+
+class MessageDialog1 {
+    public static void main(String[] args) {
+        Demo f = new Demo();
+        f.setBounds(200,200,400,300);
+        f.setResizable(true);
+        f.setVisible(true);
 
     }
 }
--- gui/src/Gui.java
+++ gui/src/Gui.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
 
-public class Gui {
+public class Gui extends JFrame {
+    Gui(){
+        JButton btn = new JButton("Click me");
+        btn.setBounds(50, 100, 80, 30);
+        add(btn);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setSize(300, 300);
+        setLayout(null);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+         new Gui();
+//        JFrame frame = new JFrame();
+//        Button btn = new Button("Click");
+//        btn.setBounds(200,200,200,200);
+//        frame.add(btn);
+//        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+//        frame.setSize(200, 200);
+//        frame.setLayout(null);
+//        frame.setVisible(true);
+
+    }
 }
