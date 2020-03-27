a9c9a2a465558c60005556f558248f9fb3578c91
jatin-js <jatinhmu@gmail.com>
2020-03-27 09:06:14 +0530

    User Exceptions

--- UserException/src/Exception.java
+++ UserException/src/Exception.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Exception {
+
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+      // float b = scr.nextDouble();   Lossy Conversion compile time error
+
+        try {
+            int i;
+        }
+    }
+
 }
